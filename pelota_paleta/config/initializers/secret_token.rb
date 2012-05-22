09d21d111c15cb5eb4e8b75eb6911379da4700b6
Xavier Gonzales <javiergosaine244@gmail.com>
2012-05-22 18:29:04 -0300

    Proyecto RoR inicial

---
+++ pelota_paleta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PelotaPaleta::Application.config.secret_token = '96ce557c59f0d9fd9f9672796c1bac92bcb46504284ae97a983ed98a14b9062f9467d7295370c43bf38e616da2b4de00227e1036ea8dd0805ff7affefbb29557'
